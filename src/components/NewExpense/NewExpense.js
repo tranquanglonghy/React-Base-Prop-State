6dd6e43c78e45131dce878d5802569c1e63e4159
tranq <longtq@smartosc.com>
2022-01-13 07:39:08 +0700

    05-react-prop-state-complete

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [formState, setFormState] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
+  const setForm = ()=>{
+    (formState === true) ? setFormState(false) : setFormState(true); 
+  }
+
+  let formContent =
+    formState === true ? (
+      <ExpenseForm onHideForm={setForm} onSaveExpenseData={saveExpenseDataHandler} />
+    ) : (
+      <button onClick={setForm}>Add New Expense</button>
+    );
+
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      {formContent}
     </div>
-  );
+    );
 };
 
 export default NewExpense;
